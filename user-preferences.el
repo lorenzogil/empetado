cc318f5d1022b91b76e35dfabb2d98f10216223c
Fidel Ramos <fidelramos@gmail.com>
2010-02-04 22:15:59 +0100

    Basic Org-mode configuration.
    Includes:
    
     * Basic agenda.
     * .org file association.
     * Anniversaries and birthdays in org-mode agenda using Emacs diary
       syntax.
     * Avoid conflict between Org-mode's priority changing keys and
       windmove's (M+arrows).

--- user-preferences.el
+++ user-preferences.el
@@ -1,4 +1,9 @@
 (setq user-mail-address "fidelramos@gmail.com")
 (setq user-full-name "Fidel Ramos Sañudo")
 
+(setq org-agenda-files (list "~/org/personal.org"
+                             "~/org/work.org"
+                             "~/org/projects.org"
+                             "~/org/anniversaries.org"))
+
 (color-theme-dark-laptop)
