c6a98bb0d245917c9a89409b9d08a548202dce7c
Lorenzo Gil <lorenzo.gil.sanchez@gmail.com>
2010-12-21 08:48:05 +0100

    Feeling like I need another fontface today

--- user-preferences.el
+++ user-preferences.el
@@ -1,8 +1,8 @@
 (setq-default user-mail-address "lorenzo.gil.sanchez@gmail.com")
 (color-theme-dark-green)
 
-(setq default-frame-alist
-      '((top . 0) (left . 0)
+;(setq default-frame-alist
+;      '((top . 0) (left . 0)
 ;       (font . "-Adobe-Courier-Medium-R-Normal--18-180-75-75-M-110-ISO8859-1")))
-        (font . "-adobe-courier-medium-r-normal--14-140-75-75-m-90-iso8859-1")))
+;        (font . "-adobe-courier-medium-r-normal--14-140-75-75-m-90-iso8859-1")))
 ;       (font . "-Adobe-Courier-Medium-R-Normal--12-120-75-75-M-150-ISO8859-1")))
